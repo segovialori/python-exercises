15f7cae06faf146c13921a445a750dbc678f9aaf
Lori Segovia <segovialori@gmail.com>
2021-01-12 14:44:18 -0600

    Data types and variables exercises 1-5

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -44,6 +44,47 @@ facebook_pay = facebook * facebook_hours
 #Sum pay from each company for total payment 
 
 total = google_pay + amazon_pay + facebook_pay
+total
 #total pay = $7420
 
-#3.  
+#3.  A student can be enrolled to a class only if the class is not full
+#  and the class schedule does not conflict with her current schedule.
+
+#First determine whether the class is full and what size would be "full"
+
+class_full = False
+schedule_conflict = False
+can_enroll = class_full == False and schedule_conflict == False
+
+
+#4.  A product offer can be applied only if people buys more than 2 items, and the offer has not expired. 
+# Premium members do not need to buy a specific amount of products.
+
+#First determine if the customer is a premium member.
+premium_member = False
+
+#If not premimum, then determine the amount of items bought.
+items = 5
+items_bought = items >= 2
+product_offer = premium_member == True or items_bought == True
+
+#5.  Create a variable that holds a boolean value for each of the following conditions:
+#the password must be at least 5 characters
+#the username must be no more than 20 characters
+#the password must not be the same as the username
+#bonus neither the username or password can start or end with whitespace
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+pass_length = len(password) >= 5
+pass_length
+username_length = len(username) <= 20
+username_length
+not_same = password != username
+not_same
+no_whitespace = 
+
+
+
+
