9c81d2a8d1843bcae9792b19ec965c19e3cdcfc5
Lori Segovia <segovialori@gmail.com>
2021-01-19 00:04:21 -0600

    vs code version of imports

---
+++ import_exercises.py
@@ -0,0 +1,6 @@
+##IMPORT EXERCISES
+#Import and test 3 of the functions from your functions exercise file.
+#Import each function in a different way:
+#- import the module and refer to the function with the . syntax
+#- use from to import the function directly
+#- use from and give the function a different name
